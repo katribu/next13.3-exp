745e78575d2898d6da73a63e564489e004ff8be8
Kat <katrinaburwash_17@hotmail.com>
2023-05-30 15:11:27 +0200

    completed winner logic

--- app/tictactoe/components/TTTBoard.tsx
+++ app/tictactoe/components/TTTBoard.tsx
@@ -2,6 +2,11 @@
 
 import { useState,useEffect } from 'react'
 
+enum Player {
+    One = 'O',
+    Two = 'X'
+}
+
 const data = [
 <div className="bg-blue-400 w-15 h-10 flex justify-center items-center"></div>,
 <div className="bg-blue-400 w-15 h-10 flex justify-center items-center"></div>,
@@ -18,6 +23,7 @@ export default function TTTBoard() {
 
     const [isPlayerOne, setIsPlayerOne] = useState<boolean>(true)
     const [gameBoard, setGameBoard] = useState<(JSX.Element | undefined)[]>(data)
+    const [whoWon, setWhoWon] = useState<string>('')
     const [winner, setWinner] = useState<boolean>(false)
 
     const handleClick = (index:number) => {
@@ -25,11 +31,11 @@ export default function TTTBoard() {
             if(i === index && isPlayerOne){
                 setIsPlayerOne(prevState => !prevState)
                 return <div className="bg-blue-400 w-15 h-10 flex justify-center items-center">
-                O</div>
+                {Player.One}</div>
             } else if (i === index && !isPlayerOne){
                 setIsPlayerOne(prevState => !prevState)
                 return <div className="bg-blue-400 w-15 h-10 flex justify-center items-center">
-                X</div>
+                {Player.Two}</div>
             } else {
                 return div
             }
@@ -39,24 +45,26 @@ export default function TTTBoard() {
     
     const checkWinner = () => {
         const mappedChildren = gameBoard.map(div => div?.props.children)
-        
-        const row1 = mappedChildren[0]==mappedChildren[1] && mappedChildren[0]==mappedChildren[2]
-        const row2 = mappedChildren[3]==mappedChildren[4] && mappedChildren[3]==mappedChildren[5]
-        const row3 = mappedChildren[6]==mappedChildren[7] && mappedChildren[6]==mappedChildren[8]
 
-        const col1 = mappedChildren[0]==mappedChildren[3] && mappedChildren[0]==mappedChildren[6]
-        const col2 = mappedChildren[1]==mappedChildren[4] && mappedChildren[1]==mappedChildren[7]
-        const col3 = mappedChildren[2]==mappedChildren[5] && mappedChildren[2]==mappedChildren[8]
-
-        const diag1 = mappedChildren[0]==mappedChildren[4] && mappedChildren[0]==mappedChildren[8]
-        const diag2 = mappedChildren[2]==mappedChildren[4] && mappedChildren[2]==mappedChildren[6]
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
         
-        console.log(mappedChildren)
-        if(row1 || row2 || row3 || col1 || col2 || col3 || diag1 || diag2){
-            setWinner(true)
-        }else {
-            return;
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (mappedChildren[a] && mappedChildren[a] === mappedChildren[b] && mappedChildren[a] === mappedChildren[c]) {
+                setWinner(true);
+                setWhoWon(mappedChildren[a]);
+            }
         }
+        return null;
 
     }
 
@@ -75,7 +83,7 @@ export default function TTTBoard() {
         <div className="grid grid-cols-3 bg-slate-900 w-1/5 mx-auto gap-3 text-white place-content-center cursor-pointer font-bold">
           {content}
         </div>
-        { winner && <h2>The winner is Player?? </h2>}
+        { winner && <h2> Congrats to Player {whoWon === 'O' ? "One" : "Two"}! You Won!!</h2>}
         </>
     )
 }
