b66ffbf07e10711c25770d8288e438b1785442fd
Kat <katrinaburwash_17@hotmail.com>
2023-05-31 15:41:47 +0200

    changed the url to http and not https

--- lib/getData.ts
+++ lib/getData.ts
@@ -1,5 +1,5 @@
  export const getData = async () => {
-    const res = await fetch('https://smashbros-unofficial-api.vercel.app/api/v1/ultimate/characters')
+    const res = await fetch('http://smashbros-unofficial-api.vercel.app/api/v1/ultimate/characters')
     if (!res.ok) {
         // This will activate the closest `error.js` Error Boundary
         throw new Error('Failed to fetch data');
